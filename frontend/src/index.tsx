3f3a4f7c1d8e952df10ec3e5816cb11ba8d2f729
kelvinghma <kelvin.ma2001@gmail.com>
2022-11-22 23:37:00 +0100

    Changes for uploading to VM

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,23 +6,25 @@ import App from "./App";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 //Connect apollo to the pokeapi endpoint
-const client = new ApolloClient({
-    uri: "http://localhost:4000/",
-    cache: new InMemoryCache(),
-});
-
-// //Connect apollo to the pokeapi endpoint
 /* const client = new ApolloClient({
-    uri: "http://it2810-40.idi.ntnu.no:4000/",
+    uri: "http://localhost:4000/",
     cache: new InMemoryCache(),
 }); */
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+// Connect apollo to the pokeapi endpoint
+const client = new ApolloClient({
+  uri: "http://it2810-40.idi.ntnu.no:4000/",
+  cache: new InMemoryCache(),
+});
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 
 root.render(
-    <React.StrictMode>
-        <ApolloProvider client={client}>
-            <App />
-        </ApolloProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
 );
